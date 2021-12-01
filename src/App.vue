e341f00c765ff8d6d2b355d95d3bc2768937b5d0
AR Refat <mdrefat382139@gmail.com>
2021-12-01 14:17:13 +0600

    class buinding

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,21 @@
 	<div v-html="hack"></div>
 	<div v-bind:id="headingId"> Hello V-bind Heading Id </div>
 	<button v-bind:disabled="buttonDisable">Button</button>
+	<div class="underline">Underline</div>
+	<div class="underline" v-bind:class="status"> Status</div>
+	<hr>
+	<h3 v-bind:class="isPromoted">This movie is Promoted</h3>
+
+	<h4 v-bind:class="isSoldOut ? 'soldOut' : 'notSoldOut' ">The Movie Sold Out </h4>
+
+	<h3 v-bind:class="['notSoldOut', 'promoted']"> Newly Promoted movie</h3>
+
+	<h2 v-bind:class="[isPromoted && 'promoted', isSoldOut ? 'soldOut' : 'notSoldOut']"> Array Conditional Class binding</h2>
+
+	<h2 v-bind:class="{
+        promoted:isPromoted,
+          soldOut:isSoldOut
+    }"> Object Conditional Class binding</h2>
 
 </template>
 
@@ -20,6 +35,9 @@ export default {
 			hack: `<a href="#" onclick="alert('your pc is hacked ! Please give me money i will save you')">Click Hear</a>`,
 			headingId: "heading",
 			buttonDisable: false,
+			status: "text-danger",
+			isPromoted: false,
+			isSoldOut: true,
 		};
 	},
 };
@@ -33,5 +51,19 @@ export default {
 	text-align: center;
 	color: #2c3e50;
 	margin-top: 60px;
+}
+.underline {
+	text-decoration: underline;
+	font-size: 50px;
+}
+.promoted {
+	font-family: inherit;
+	font-style: italic;
+}
+.soldOut {
+	color: rgb(221, 14, 14);
+}
+.notSoldOut {
+	color: rgb(28, 165, 28);
 }
 </style>
